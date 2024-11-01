31f6383e178c8f65abfb1a1ee26ce5cd052010d4
elmira <gilfanova-03@inbox.ru>
2024-11-01 22:59:13 +0300

    ok

---
+++ src/map/s21_map.h
@@ -0,0 +1,79 @@
+#ifndef S21_MAP_H
+#define S21_MAP_H
+
+#include <vector>
+
+#include "s21_tree.h"
+
+namespace s21 {
+
+template <typename K, typename V>
+class Map {
+ public:
+  using key_type = K;
+  using mapped_type = V;
+  using value_type = std::pair<const key_type, mapped_type>;
+  using size_type = size_t;
+  using iterator = typename Tree<key_type, mapped_type>::iterator;
+
+  Map() : tree_() {}
+  Map(std::initializer_list<value_type> init) : tree_(init) {}
+  Map(const Map &other) : tree_(other.tree_) {}
+  Map(Map &&other) noexcept : tree_(std::move(other.tree_)) {}
+  ~Map() = default;
+
+  Map &operator=(const Map &other) {
+    if (this != &other) {
+      tree_ = other.tree_;
+    }
+    return *this;
+  }
+
+  Map &operator=(Map &&other) noexcept {
+    if (this != &other) {
+      tree_ = std::move(other.tree_);
+    }
+    return *this;
+  }
+
+  mapped_type &at(const key_type &key) { return tree_.at(key); }
+  mapped_type &operator[](const key_type &key) { return tree_[key]; }
+
+  bool empty() const noexcept { return tree_.empty(); }
+  size_type size() const noexcept { return tree_.size(); }
+  size_type max_size() const noexcept { return tree_.max_size(); }
+
+  std::pair<iterator, bool> insert(const value_type &value) {
+    return tree_.insert(value);
+  }
+
+  std::pair<iterator, bool> insert_or_assign(const value_type &value) {
+    return tree_.insert_or_assign(value);
+  }
+
+  void erase(iterator pos) { tree_.erase(pos); }
+
+  void clear() noexcept { tree_.clear(); }
+
+  iterator find(const key_type &key) { return tree_.find_pos(key); }
+  bool contains(const key_type &key) const noexcept {
+    return tree_.contains(key);
+  }
+
+  iterator begin() const { return tree_.begin(); }
+  iterator end() const { return tree_.end(); }
+
+  void swap(Map &other) { tree_.swap(other.tree_); }
+
+  template <typename... Args>
+  std::vector<std::pair<iterator, bool>> insert_many(Args &&...args) {
+    return tree_.insert_many(std::forward<Args>(args)...);
+  }
+
+ private:
+  Tree<key_type, mapped_type> tree_;
+};
+
+}  // namespace s21
+
+#endif  // S21_MAP_H
--- src/set/s21_set.h
+++ src/set/s21_set.h
@@ -5,15 +5,31 @@
 #include <limits>
 namespace s21
 {
-    template <typename T>
+    template <typename K>
     class Set
     {
     private:
         
 
     public:
+    using key_type = K;
+    using value_type = K;
+    using reference = K &;
+    using const_reference = const K &;
+    using size_type = size_t;
+    //iterator
+    //const_iterator
+
+    Set() : tree_() {}
+    Set(std::initializer_list<value_type> const &items) tree_(init) {}
+
+
+
+
     };
+
+
 }
-#include "s21_set.tpp"
+
 
 #endif
